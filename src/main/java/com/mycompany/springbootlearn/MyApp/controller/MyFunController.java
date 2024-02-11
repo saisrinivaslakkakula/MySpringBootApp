53105e80bcacf2d86d7ed3ad8057cd6ef4b6c3b6
Sai Srinivas Lakkakula <saisrinivaslakkakula@Sais-MacBook-Pro.local>
2024-02-10 19:38:21 -0800

    Injecting application.properties into Controller using @Value annotation. using the injected properties in the controller

--- src/main/java/com/mycompany/springbootlearn/MyApp/controller/MyFunController.java
+++ src/main/java/com/mycompany/springbootlearn/MyApp/controller/MyFunController.java
@@ -1,14 +1,21 @@
 package com.mycompany.springbootlearn.MyApp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyFunController {
 
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
     @GetMapping("/")
     public String greetUser() {
-        return "Hello!";
+        return String.format("Hello %s ! your team %s is ready. Good Luck!", coachName, teamName);
     }
 
     @GetMapping("/workout")
